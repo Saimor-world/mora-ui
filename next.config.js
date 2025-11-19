61bb10d911b6b3cd458dd9bca33ed4abae8fa788
Dein Name <dein@email.tld>
2025-11-19 16:59:11 +0100

    fix: move allowed dev origins config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    allowedDevOrigins: [
-      "http://localhost:3002",
-      "http://127.0.0.1:3002",
-      "http://172.20.10.*",
-      "https://*.trycloudflare.com",
-    ],
-  },
+  allowedDevOrigins: [
+    "http://localhost:3002",
+    "http://127.0.0.1:3002",
+    "http://172.20.10.*",
+    "https://*.trycloudflare.com",
+  ],
 };
 
 module.exports = nextConfig;
